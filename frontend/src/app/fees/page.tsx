c487cf163d7f74c96d76dffd7f8fc78a860e7b57
acadlyx <info.acadlyx@gmail.com>
2026-09-20 00:18:29 +0530

    Create page.tsx

---
+++ frontend/src/app/fees/page.tsx
@@ -0,0 +1,13 @@
+import { DashboardShell } from "@/components/dashboard/DashboardShell";
+import Phase1ModulePage from "@/components/phase1/Phase1ModulePage";
+
+export default function Page() {
+  return (
+    <DashboardShell
+      title="Student Fees"
+      subtitle="ACADLYX Core ERP"
+    >
+      <Phase1ModulePage module="fees" />
+    </DashboardShell>
+  );
+}
