3f1a418a3ef70f5b426267339e55e3da81f794a2
acadlyx <info.acadlyx@gmail.com>
2026-09-20 00:20:00 +0530

    Create page.tsx

---
+++ frontend/src/app/notifications/page.tsx
@@ -0,0 +1,13 @@
+import { DashboardShell } from "@/components/dashboard/DashboardShell";
+import Phase1ModulePage from "@/components/phase1/Phase1ModulePage";
+
+export default function Page() {
+  return (
+    <DashboardShell
+      title="Notifications Center"
+      subtitle="ACADLYX Core ERP"
+    >
+      <Phase1ModulePage module="notifications" />
+    </DashboardShell>
+  );
+}
